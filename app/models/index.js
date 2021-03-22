fa04ecb7ab46f28300629fc183a7423c1ac0b9e1
elif-bunny <robdev91@gmail.com>
2021-03-22 13:30:50 -0400

    fix model sequelize

--- app/models/index.js
+++ app/models/index.js
@@ -18,6 +18,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.jobs = require("./job.model.js")(sequelize);
+db.jobs = require("./job.model.js")(sequelize, Sequelize);
 
 module.exports = db;
