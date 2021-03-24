522a896b9a810996df8e313cebdb8bb32f3fabdd
elif-bunny <robdev91@gmail.com>
2021-03-24 14:14:17 -0400

    fix skill request issue

--- app/controllers/skill.controller.js
+++ app/controllers/skill.controller.js
@@ -3,8 +3,8 @@ const db = require('../models')
 const Skills = db.skills
 
 // Create and Save a new Job
-exports.findAll = (res) => {
+exports.findAll = (_req, res) => {
   Skills.findAll()
-    .then(data => res.send({ data }))
+    .then(data => res.send({ skills: data.map(skill => { return { id: skill.id, name: skill.name } }) }))
     .catch(err => res.send({ message: err.message }))
 }
