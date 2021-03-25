b7c07c8b7d3b5d35493067283c6d12a9ece71e8d
elif-bunny <robdev91@gmail.com>
2021-03-24 22:07:04 -0400

    change port to 8000

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ db.sequelize.sync().then(_ => {
 require('./app/routes')(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
